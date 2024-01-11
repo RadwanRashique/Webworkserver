c218d049d213093421fa651db74a76e874b84389
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-01-11 10:15:15 +0530

    s

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors");
 app.use(cors({
-  origin: "https://webworkfront.vercel.app",
+  origin: "https://webwork-sigma.vercel.app/",
 }));
 // to use (.env file access)
 require('dotenv').config()
